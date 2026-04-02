7d486736e40f6f4fa28c0fd075fd846cb7d8f0ae
hackernet01 <cyberdepart0101@gmail.com>
2026-04-02 15:54:15 +0500

    Update list.js

--- api/list.js
+++ api/list.js
@@ -1,20 +1,21 @@
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     
-    const url = 'https://rich-dory-74324.upstash.io';
-    const token = 'gQAAAAAAASJUAAIncDEzN2E4MTAzZjIwZTA0NmNkODJjNDY0OWQ5OGJhYzlkZnAxNzQzMjQ';
+    const UPSTASH_URL = 'https://rich-dory-74324.upstash.io';
+    const UPSTASH_TOKEN = 'gQAAAAAAASJUAAIncDEzN2E4MTAzZjIwZTA0NmNkODJjNDY0OWQ5OGJhYzlkZnAxNzQzMjQ';
     
     try {
-        const keysRes = await fetch(`${url}/keys/*`, {
-            headers: { 'Authorization': `Bearer ${token}` }
+        // Get all keys
+        const keysRes = await fetch(`${UPSTASH_URL}/keys/*`, {
+            headers: { 'Authorization': `Bearer ${UPSTASH_TOKEN}` }
         });
         const keys = await keysRes.json();
         
         const uids = [];
         if (keys.result && keys.result.length > 0) {
             for (const key of keys.result) {
-                const dataRes = await fetch(`${url}/get/${key}`, {
-                    headers: { 'Authorization': `Bearer ${token}` }
+                const dataRes = await fetch(`${UPSTASH_URL}/get/${key}`, {
+                    headers: { 'Authorization': `Bearer ${UPSTASH_TOKEN}` }
                 });
                 const data = await dataRes.json();
                 if (data.result) {
