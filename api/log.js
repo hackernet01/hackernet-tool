4b03eff067e25a9523f4067e08480a2156a725ab
hackernet01 <cyberdepart0101@gmail.com>
2026-04-02 13:38:46 +0500

    Update log.js

--- api/log.js
+++ api/log.js
@@ -28,7 +28,9 @@ export default async function handler(req, res) {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ chat_id: ADMIN_ID, text: message })
         });
-    } catch(e) {}
+    } catch(e) {
+        console.error('Telegram error:', e);
+    }
     
     return res.status(200).json({ success: true });
 }
